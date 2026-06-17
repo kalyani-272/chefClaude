d5791551a8d860c1148517da9849b4a0c779f4f7
kalyani-272 <sri.kalyani272@gmail.com>
2026-06-18 03:42:40 +0530

    Updated project

--- chefClaude/src/App.jsx
+++ chefClaude/src/App.jsx
@@ -1,5 +1,9 @@
 import Header from "./Header"
 import MainContent from "./MainContent"
+import IngredientsList from "./components/IngredientsList"
+import ClaudeRecipe from "./components/ClaudeRecipe.jsx"
+
+
 export default function App() {
   return (
     <div className="App">
--- chefClaude/src/main.jsx
+++ chefClaude/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import './App.css'
 import './header.jsx'
 import './MainContent.jsx'
+import IngredientsList from "./components/IngredientsList"
+import ClaudeRecipe from "./components/ClaudeRecipe.jsx"
+import { getRecipeFromMistral } from './ai.js'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
